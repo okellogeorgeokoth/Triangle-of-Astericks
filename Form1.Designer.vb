db9bb95267674caf6fb778a877261ef3eb7f9415
okellogeorgeokoth <okoth603@gmail.com>
2022-02-21 01:41:51 +0300

    Triangle-of-Astericks

---
+++ Form1.Designer.vb
@@ -0,0 +1,55 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()>
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()>
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()>
+    Private Sub InitializeComponent()
+        Me.outputTextBox = New System.Windows.Forms.TextBox()
+        Me.SuspendLayout()
+        '
+        'outputTextBox
+        '
+        Me.outputTextBox.BackColor = System.Drawing.SystemColors.Highlight
+        Me.outputTextBox.Font = New System.Drawing.Font("Lucida Console", 12.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.outputTextBox.Location = New System.Drawing.Point(13, 13)
+        Me.outputTextBox.Multiline = True
+        Me.outputTextBox.Name = "outputTextBox"
+        Me.outputTextBox.ScrollBars = System.Windows.Forms.ScrollBars.Vertical
+        Me.outputTextBox.Size = New System.Drawing.Size(477, 403)
+        Me.outputTextBox.TabIndex = 0
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(7.0!, 15.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.BackColor = System.Drawing.SystemColors.ActiveCaption
+        Me.ClientSize = New System.Drawing.Size(501, 427)
+        Me.Controls.Add(Me.outputTextBox)
+        Me.Font = New System.Drawing.Font("Segoe UI", 9.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Name = "Form1"
+        Me.Text = "Triangle of Astericks"
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+
+    Friend WithEvents outputTextBox As TextBox
+End Class
---
+++ Form1.vb
@@ -0,0 +1,111 @@
+﻿'Name
+'Details
+Public Class Form1
+    Private Sub Form1_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+
+        For counterVar As Integer = 1 To 10 Step 1      ' Printing the asterisks in inner loop
+
+            For innerCounter As Integer = 1 To counterVar Step 1
+
+                ' Statement to print the asterisk
+
+                outputTextBox.AppendText("*")
+
+            Next
+
+            ' Adding new line statements
+
+            outputTextBox.AppendText(vbCrLf)
+
+        Next
+
+        ' Adding Space between patterns
+
+        outputTextBox.AppendText(vbCrLf)
+
+        ' Loop used to display the second pattern
+
+        For counterVar As Integer = 1 To 10 Step 1
+
+            For innerCounter As Integer = 10 To counterVar Step -1
+                ' Printing asterick statement
+                outputTextBox.AppendText("*")
+
+            Next
+
+            ' Adding new line statements
+
+            outputTextBox.AppendText(vbCrLf)
+
+        Next
+
+        ' Adding Space between patterns
+
+        outputTextBox.AppendText(vbCrLf)
+
+        ' Loop displaying the third pattern
+
+        For counterVar As Integer = 1 To 10 Step 1
+
+            ' Loop printing the leading spaces
+
+            For innerSpaceCounter As Integer = 1 To (counterVar - 1) Step 1
+
+                ' Statement  printing the leading spaces
+
+                outputTextBox.AppendText(" ")
+
+            Next
+
+            ' Loop for printing the asterisks
+
+            For innerCounter As Integer = 10 To counterVar Step -1
+
+                ' Statement for printing the asterisk
+
+                outputTextBox.AppendText("*")
+
+            Next
+
+            ' Statement to add a new line
+
+            outputTextBox.AppendText(vbCrLf)
+
+        Next
+
+        ' leave a line space between patterns
+
+        outputTextBox.AppendText(vbCrLf)
+
+        ' Loop  displaying the fourth pattern
+
+        For counterVar As Integer = 1 To 10 Step 1
+
+            ' loop for printing the leading spaces
+
+            For innerSpaceCounter As Integer = 10 To (counterVar + 1) Step -1
+
+                ' statement to print the leading spaces
+
+                outputTextBox.AppendText(" ")
+
+            Next
+
+            ' loop for printing the asterisks
+
+            For innerCounter As Integer = 1 To counterVar Step 1
+
+                '  print the asterisk statement
+
+                outputTextBox.AppendText("*")
+
+            Next
+
+            ' add a new line statement
+
+            outputTextBox.AppendText(vbCrLf)
+
+        Next
+
+    End Sub ' MyForm_Load procedure ends here
+End Class  'end of Class MyForm 
